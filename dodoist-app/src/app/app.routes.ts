a0b73bcb6b5b162daf099d39d748733d7cb00fe1
Ariuna Bairova <bairova.at@phystech.edu>
2026-03-14 15:12:49 +0300

    Create landing page

---
+++ dodoist-app/src/app/app.routes.ts
@@ -0,0 +1,26 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('./pages/landing/landing.component').then(m => m.LandingComponent),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login.component').then(m => m.LoginComponent),
+  },
+  {
+    path: 'signup',
+    loadComponent: () =>
+      import('./pages/signup/signup.component').then(m => m.SignupComponent),
+  },
+  {
+    path: 'docs',
+    loadComponent: () =>
+      import('./pages/docs/docs.component').then(m => m.DocsComponent),
+  },
+  { path: '**', redirectTo: '' },
+];
